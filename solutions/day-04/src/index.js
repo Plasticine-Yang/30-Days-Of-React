6f9d1ea242d3fe9eb21d35a2f0903c0bc96219fe
plasticine9750 <975036719@qq.com>
2022-06-17 17:51:41 +0800

    chore(day-04): level2 case2

--- solutions/day-04/src/index.js
+++ solutions/day-04/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-import Level2 from './level2.jsx';
+import { Level2Case1, Level2Case2 } from './level2.jsx';
 
 import './style.css';
 
@@ -10,9 +10,17 @@ const rootElement = document.getElementById('root');
 const App = () => {
   return (
     <div className="app">
+      {/* Level2 */}
       <div>
         <h1>Level2</h1>
-        <Level2 />
+
+        {/* Case 1 */}
+        <p>Case 1</p>
+        <Level2Case1 />
+
+        {/* Case 2 */}
+        <p>Case 2</p>
+        <Level2Case2 />
       </div>
     </div>
   );
--- solutions/day-04/src/level2.jsx
+++ solutions/day-04/src/level2.jsx
@@ -3,9 +3,9 @@ import cssLogoImg from './images/css_logo.png';
 import jsLogoImg from './images/js_logo.png';
 import reactLogoImg from './images/react_logo.png';
 
-const Level2 = () => {
+export const Level2Case1 = () => {
   return (
-    <section className="level2-container">
+    <section className="level2-case1-container">
       <p>Front End Technologies</p>
       <div className="img-container">
         <img src={htmlLogoImg} alt="html" />
@@ -17,4 +17,17 @@ const Level2 = () => {
   );
 };
 
-export default Level2;
+export const Level2Case2 = () => {
+  return (
+    <section className="level2-case2-container">
+      <h1>SUBSCRIBE</h1>
+      <p>Sign up with your email address to receive news and updates.</p>
+      <div className="field-container">
+        <input className="field" placeholder="First name" type="text" />
+        <input className="field" placeholder="Last name" type="text" />
+        <input className="field" placeholder="Email name" type="text" />
+      </div>
+      <button className="subscribe-btn">Subscribe</button>
+    </section>
+  );
+};
